28df687f578a8f29ba1f1215ec263a8df920af6a
Michał Rudowicz <michal.rudowicz@fl9.eu>
2016-11-26 15:38:35 +0100

    Remade builder pattern

--- Builder/src/main.cpp
+++ Builder/src/main.cpp
@@ -3,20 +3,11 @@
 
 int main(void)
 {
-    bool isDesktop = true;
-    std::cout << "Is it a desktop computer? 1 if so, 0 otherwise." << std::endl;
-    std::cin >> isDesktop;
     std::string page;
-    if (isDesktop)
-    {
-        page.append("Desktop header: Welcome to my website");
-        page.append("Desktop paragraph: This website is under construction.");
-    }
-    else
-    {
-        page.append("Mobile header: Welcome to my website");
-        page.append("Mobile paragraph: This website is under construction.");
-    }
+    page.append("<html><body>");
+    page.append("<h1>Welcome to my website</h1>");
+    page.append("<p>This website is under construction.</p>");
+    page.append("</body></html>");
     std::cout << page;
     return 0;
 }
